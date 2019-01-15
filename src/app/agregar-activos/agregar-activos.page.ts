e2b0d09c0fc3143bcc5ea6e2d1fdff8fdcbeef5a
tihomir22 <tihomir_alcudia3@hotmail.com>
2019-01-15 21:52:32 +0100

    ALPHA 0.1.1 : Arreglada la busqueda de activos, implementado botones de mostrar más y/o mostrar menos junto a su funcionabilidad,mejoras de interfaz, añadido de nuevos activos (stocks) en la BBDD

--- src/app/agregar-activos/agregar-activos.page.ts
+++ src/app/agregar-activos/agregar-activos.page.ts
@@ -12,7 +12,7 @@ import { HammerGestureConfig } from "@angular/platform-browser";
   templateUrl: './agregar-activos.page.html',
   styleUrls: ['./agregar-activos.page.scss'],
 })
-export class AgregarActivosPage extends HammerGestureConfig{
+export class AgregarActivosPage extends HammerGestureConfig {
   buildHammer(element: HTMLElement) {
     const mc = new (<any>window).Hammer(element);
 
@@ -24,62 +24,82 @@ export class AgregarActivosPage extends HammerGestureConfig{
 
     return mc;
   }
-  data:Array<any>=new Array;
-  
-  searchTerm:string;
- 
-  copiaData:Array<any>=new Array;
-  activos_cripto:Array<any>=new Array;
-  activos_stock:Array<any>=new Array;
+  data: Array<any> = new Array;
+
+  searchTerm: string;
+
+  copiaData: Array<any> = new Array;
+  activos_cripto: Array<any> = new Array;
+  activos_stock: Array<any> = new Array;
   searching: any = false;
-  
+  startPage: Number;
+  paginationLimit: Number;
+  startPageStocks: Number;
+  paginationLimitStocks: Number;
 
-  private baseURI               : string  = "http://dembow.gearhostpreview.com/";
+
+  private baseURI: string = "http://dembow.gearhostpreview.com/";
 
 
-  constructor(public alertController: AlertController, public service:ServiceLoginDashboardService,public http:HttpClient,public toastCtrl:ToastController,public navCtrl:NavController,public loadingCtrl: LoadingController) {
+  constructor(public alertController: AlertController, public service: ServiceLoginDashboardService, public http: HttpClient, public toastCtrl: ToastController, public navCtrl: NavController, public loadingCtrl: LoadingController) {
     super();
-    this.data=service.getDestn();
-
+    this.data = service.getDestn();
+    this.startPage = 0;
+    this.paginationLimit = 5;
+    this.startPageStocks = 0;
+    this.paginationLimitStocks = 5;
   }
 
   ngOnInit() {
-    this.service.cargarActivos().subscribe(async (  data : any) =>
-    {
-       console.dir(data);
-       if(data==null || data==false){
+    this.service.cargarActivos().subscribe(async (data: any) => {
+      console.dir(data);
+      if (data == null || data == false) {
         this.sendNotification("No existen activos...");
-       }else{
-         await this.service.actualizarActivos(data)
+      } else {
+        await this.service.actualizarActivos(data)
         this.clasificarResultado(data);
-       }
+      }
     },
-    (error : any) =>
-    {
-      this.sendNotification("Hubo un error inesperado...");
-    });;
+      (error: any) => {
+        this.sendNotification("Hubo un error inesperado...");
+      });;
   }
- 
-  
-  
- 
- 
-  filtrarActivos(){
-    this.searching=true;
-    let array:Array<any>=new Array;
+
+
+  showMoreItems(opcion: string) {
+    if (opcion == "cripto") {
+      this.paginationLimit = Number(this.paginationLimit) + 3;
+    } else if (opcion == "stocks") {
+      this.paginationLimitStocks = Number(this.paginationLimitStocks) + 3;
+    }
+  }
+  showLessItems(opcion: string) {
+    if (opcion == "cripto") {
+      this.paginationLimit = Number(this.paginationLimit) - 3;
+    } else if (opcion == "stocks") {
+      this.paginationLimitStocks = Number(this.paginationLimitStocks) - 3;
+    }
+  }
+
+
+
+
+  filtrarActivos() {
+    this.searching = true;
+    let array: Array<any> = new Array;
     this.copiaData.forEach(element => {
-      let nombre:string=element.nombre;
+      let nombre: string = element.nombre;
       console.dir(nombre);
-      if(nombre.toLocaleLowerCase().includes(this.searchTerm)){
-        console.dir(nombre + " contiene "+ this.searchTerm);
+      if (nombre.toLowerCase().includes(this.searchTerm.toLowerCase())) {
+        console.dir(nombre + " contiene " + this.searchTerm);
         array.push(element);
       }
     });
     console.dir(array.length);
     this.clasificarResultado(array);
-    
+
   }
-  async presentAlert(activo:any) {
+  async presentAlert(activo: any) {
     const alert = await this.alertController.create({
       header: activo.nombre,
       subHeader: activo.tipo,
@@ -90,70 +110,69 @@ export class AgregarActivosPage extends HammerGestureConfig{
     await alert.present();
   }
 
- 
- 
-  generarImgCripto(activo){
-     return this.baseURI+"img-activos/"+activo.nombre+".png";
+
+
+  generarImgCripto(activo) {
+    return this.baseURI + "img-activos/" + activo.nombre + ".png";
   }
-  generarImgStock(activo){
-    return this.baseURI+"img-activos-stocks/"+activo.nombre+".png";
- }
- 
+  generarImgStock(activo) {
+    return this.baseURI + "img-activos-stocks/" + activo.nombre + ".png";
+  }
 
-  clasificarResultado(data:any){
-    if(this.copiaData.length==0){
-      this.copiaData=data;
+
+  clasificarResultado(data: any) {
+    if (this.copiaData.length == 0) {
+      this.copiaData = data;
     }
-    if(this.activos_cripto.length>0){
-      this.activos_cripto.length=0;
+    if (this.activos_cripto.length > 0) {
+      this.activos_cripto.length = 0;
     }
-    if(this.activos_stock.length>0){
-      this.activos_stock.length=0;
+    if (this.activos_stock.length > 0) {
+      this.activos_stock.length = 0;
     }
 
-    this.data=data;
+    this.data = data;
     data.forEach(element => {
-      if (element.tipo=="Criptomoneda") {
+      if (element.tipo == "Criptomoneda") {
         this.activos_cripto.push(element);
       }
-      if(element.tipo=="Stock"){
+      if (element.tipo == "Stock") {
         this.activos_stock.push(element);
       }
     });
-    this.searching=false;
+    this.searching = false;
   }
-  abrirDetalles(activo:any){
+  abrirDetalles(activo: any) {
     this.service.setActivo(activo);
     this.service.setTipoAdquisicion("indeterminada");
     this.navCtrl.navigateForward("/detalles-activo");
     console.dir(activo.nombre);
   }
-  comprar(item:any){
+  comprar(item: any) {
     this.service.setActivo(item);
     this.service.setTipoAdquisicion("comprar");
     this.navCtrl.navigateForward("/detalles-activo");
     console.dir(item.nombre);
   }
-  vender(item:any){
+  vender(item: any) {
     this.service.setActivo(item);
     this.service.setTipoAdquisicion("vender");
     this.navCtrl.navigateForward("/detalles-activo");
     console.dir(item.nombre);
   }
-  buscar(){
-    console.dir("vamos a buscar loco"+ this.searchTerm);
+  buscar() {
+    console.dir("vamos a buscar loco" + this.searchTerm);
   }
-  mostrarDescripcion(activo:any){
+  mostrarDescripcion(activo: any) {
     console.dir(activo.descripcion);
   }
 
-  async sendNotification(message : string)
-  {
-     let toast = await this.toastCtrl.create({
-         message       : message,
-         duration      : 3000
-     });
-     toast.present();
+  async sendNotification(message: string) {
+    let toast = await this.toastCtrl.create({
+      message: message,
+      duration: 3000
+    });
+    toast.present();
   }
 
 }
